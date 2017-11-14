f699fbb159ce8fc5563c9c9705eb3bb63ed29c34
Baconator <yildri@yildri.nl>
2017-11-14 15:59:16 +0100

    Remove redundant code

--- app/src/main/java/nl/yildri/droidule/Util/DatabaseOpenHelper.java
+++ app/src/main/java/nl/yildri/droidule/Util/DatabaseOpenHelper.java
@@ -12,17 +12,14 @@ public class DatabaseOpenHelper extends SQLiteOpenHelper
     private static final String DATABASE_NAME = "droidule";
     private static final int DATABASE_VERSION = 1;
 
-    private Context context;
-
     public DatabaseOpenHelper(Context context)
     {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-
-        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        //Doesn't appear to get called for some reason, but still needs to exists for inheritance.
     }
 
     @Override
